7ecbe19337c93f3db266bd612e179dccb9a5a84b
gakor <akorgodwin01@gmail.com>
2026-07-03 15:05:47 +0100

    handling of get request method

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import(
+import (
 	"fmt"
+	"net/http"
 )
 
-func CalculateBMI(){
-	
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "welcome to my golang server")
+}
+
+func submit(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "this method is not valid", http.StatusMethodNotAllowed)
+	}
+	return
+}
+
+func main() {
+
+	http.HandleFunc("/", home)
+	http.HandleFunc("/submit", submit)
+
+	fmt.Println("server running on port :9090")
+	http.ListenAndServe(":9090", nil)
 }
